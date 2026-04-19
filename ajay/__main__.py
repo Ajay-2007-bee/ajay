29baf11d45f0254fc2cbba4fce25dc783c60a3c2
Ajay-2007-bee <ajayiyanraj.2401007@srec.ac.in>
2026-04-19 21:00:52 +0530

    cleanup

--- ajay/__main__.py
+++ ajay/__main__.py
@@ -4,10 +4,13 @@ from . import getcode
 def main():
     if len(sys.argv) < 3:
         print("Usage: ajay <algorithm> <py/c>")
-        print("Example: ajay dijkstra py")
         return
 
     name = sys.argv[1]
     lang = sys.argv[2]
 
-    print(getcode(name, lang))
+    result = getcode(name, lang)
+    print(result)
+
+if __name__ == "__main__":
+    main()
